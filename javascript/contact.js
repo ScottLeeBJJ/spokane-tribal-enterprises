11600de351b2428ad4aad59b20034c484cadb519
ScottLeeBJJ <scottlee5455@outlook.com>
2020-08-21 20:42:46 -0700

    imported the correct photos for the project

--- javascript/contact.js
+++ javascript/contact.js
@@ -1,3 +1,11 @@
 function myFunction() {
     var input = document.getElementById("contact").onsubmit();
+
+    function validateForm() {
+        var x = document.forms["myForm"]["fname"].value;
+        if (x == "") {
+          alert("Name must be filled out");
+          return false;
+        }
+      }
 }
