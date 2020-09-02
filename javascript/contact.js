449c2a390ffadd426de346fb0ee0b06ceedf9c43
ScottLeeBJJ <scottlee5455@outlook.com>
2020-09-02 08:09:32 -0700

    finished the media queries

--- javascript/contact.js
+++ javascript/contact.js
@@ -2,7 +2,7 @@ function myFunction() {
     var input = document.getElementById("contact").onsubmit();
 
     function validateForm() {
-        var x = document.forms["myForm"]["fname"].value;
+      var x = document.forms["myForm"]["fname"].value;
         if (x == "") {
           alert("Name must be filled out");
           return false;
